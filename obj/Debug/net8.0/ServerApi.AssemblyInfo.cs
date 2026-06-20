e8aecf1905871f3c2c71c565e8507ca664d0060e
Baby Nap Boltron <babynap8@gmail.com>
2026-06-20 21:11:34 +0800

    Debug

--- obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a049099414e0b671335d7522a018bc123e7efc23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
